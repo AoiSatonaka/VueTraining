e97bceda8c214e9f7189b741b508c6ba18b7e83a
AoiSatonaka <fireball0821@icloud.com>
2020-08-19 19:17:35 +0900

    [add] address book app setup

---
+++ addressbook/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar app color="#48bc58">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <span>マイアドレス帳</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+
+    </v-app-bar>
+
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
